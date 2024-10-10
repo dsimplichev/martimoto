489584211b05adf6d137eee3759423eb46cccfe6
dsimplichev <dsimplichev1948@gmail.com>
2024-10-10 18:10:46 +0300

    start working on new page/ search

---
+++ src/pages/search/Search.jsx
@@ -0,0 +1,14 @@
+function Search() {
+    return (
+        <>
+            <div className="page-content">
+                <div className="image-container">
+                    <button className="search-btn">Search</button>
+                    <p className="search-text">Find what you need</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Search;
