b9d4085de688b30f872542d5a86e1f45d9cabf6d
dsimplichev <dsimplichev1948@gmail.com>
2024-10-10 19:00:34 +0300

    styling done search page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Route, Routes, Router } from "react-router-dom";
 import Nav from "./pages/navbar/Nav";
+import Search from "./pages/search/Search";
 
 
 
 function App() {
   return (
+    <>
     <Nav/>
+    <Search />
+    </>
   );
 }
 
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,10 +1,18 @@
+import img from "../../assets/img_hme_11.jpg"
+import "./search.css"
+
 function Search() {
     return (
         <>
             <div className="page-content">
                 <div className="image-container">
-                    <button className="search-btn">Search</button>
-                    <p className="search-text">Find what you need</p>
+                    <img className="img" src={img} alt='search-moto' />
+                    <div className="search-container">
+                        
+                        <p className="shop-text">SHOP</p>
+                        <p className="search-text">Find what you need</p>
+                        <button className="search-btn">Search</button>
+                    </div>
                 </div>
             </div>
         </>
