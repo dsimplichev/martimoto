67515c9f98fbbaf5932128949c4e7f41d0750060
dsimplichev <dsimplichev1948@gmail.com>
2024-10-15 15:49:38 +0300

    working on last products / css styling

--- src/pages/lastproduct/LastProduct.jsx
+++ src/pages/lastproduct/LastProduct.jsx
@@ -11,6 +11,40 @@ function LastProduct() {
             </div>
 
             <div className="divider"></div>
+
+            <div className="products-grid">
+                <div className="product">
+                    <img src="path_to_image" alt="Product 1" />
+                    <div className="product-buttons">
+                        <button className="details-btn">Details</button>
+                        <button className="add-to-cart-btn">Add to cart</button>
+                    </div>
+                </div>
+
+                <div className="product">
+                    <img src="path_to_image" alt="Product 2" />
+                    <div className="product-buttons">
+                        <button className="details-btn">Details</button>
+                        <button className="add-to-cart-btn">Add to cart</button>
+                    </div>
+                </div>
+
+                <div className="product">
+                    <img src="path_to_image" alt="Product 3" />
+                    <div className="product-buttons">
+                        <button className="details-btn">Details</button>
+                        <button className="add-to-cart-btn">Add to cart</button>
+                    </div>
+                </div>
+
+                <div className="product">
+                    <img src="path_to_image" alt="Product 4" />
+                    <div className="product-buttons">
+                        <button className="details-btn">Details</button>
+                        <button className="add-to-cart-btn">Add to cart</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
