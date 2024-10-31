887eaee05dceb1e1879a80de442e8f85107266b6
dsimplichev <dsimplichev1948@gmail.com>
2024-10-31 16:57:20 +0200

    need work on searchParts

--- src/pages/partsSearch/PartsSearch.jsx
+++ src/pages/partsSearch/PartsSearch.jsx
@@ -5,55 +5,56 @@ import './PartsSearch.css';
 function PartsSearch() {
     return (
         <div className="parts-search-container">
-        
 
-        <div className="filter-buttons">
-            <button className="filter-button">NEW</button>
-            <button className="filter-button">USED</button>
-            <button className="filter-button">SHOP ALL</button>
-        </div>
-        
-        <div className="search-now">
-        <div className="search-title-container">
-        <h2 className="search-title">SEARCH NOW</h2>
-        </div>
-        <form className="search-form">
-            <div className="form-group">
-                
-                <select id="year" name="year">
-                    <option  value="">Year</option>
-                    <option value="2024">2024</option>
-                    <option value="2023">2023</option>
-                   
-                </select>
+
+            <div className="filter-buttons">
+                <button className="filter-button">NEW</button>
+                <button className="filter-button">USED</button>
+                <button className="filter-button">SHOP ALL</button>
             </div>
 
-            <div className="form-group">
-                
-                <select id="make" name="make">
-                    <option value="">Make</option>
-                    <option value="make1">Make 1</option>
-                    <option value="make2">Make 2</option>
-                   
-                </select>
-            </div>
+            <div className="search-now">
+                <div className="search-title-container">
+                    <h2 className="search-title">SEARCH NOW</h2>
+                    <div className="divider-search"></div>
+                </div>
+                <form className="search-form">
+                    <div className="form-group">
 
-            <div className="form-group">
-                
-                <select id="model" name="model">
-                    <option value="">Model</option>
-                    <option value="model1">model 1</option>
-                    <option value="model2">model 2</option>
-                    
-                </select>
-            </div>
+                        <select id="year" name="year">
+                            <option value="">Year</option>
+                            <option value="2024">2024</option>
+                            <option value="2023">2023</option>
 
-            <button type="submit" className="search-button">Search</button>
-        </form>
+                        </select>
+                    </div>
+
+                    <div className="form-group">
+
+                        <select id="make" name="make">
+                            <option value="">Make</option>
+                            <option value="make1">Make 1</option>
+                            <option value="make2">Make 2</option>
+
+                        </select>
+                    </div>
+
+                    <div className="form-group">
+
+                        <select id="model" name="model">
+                            <option value="">Model</option>
+                            <option value="model1">model 1</option>
+                            <option value="model2">model 2</option>
+
+                        </select>
+                    </div>
+
+                    <button type="submit" className="search-button">Search</button>
+                </form>
+            </div>
         </div>
-    </div>
-);
-  
+    );
+
 }
 
 export default PartsSearch;
