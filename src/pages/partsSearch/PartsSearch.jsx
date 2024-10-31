ee8a1c537216b66e63307836283b7540bb37402c
dsimplichev <dsimplichev1948@gmail.com>
2024-10-31 15:53:57 +0200

    working on styling

--- src/pages/partsSearch/PartsSearch.jsx
+++ src/pages/partsSearch/PartsSearch.jsx
@@ -5,15 +5,27 @@ import './PartsSearch.css';
 function PartsSearch() {
     return (
         <div className="parts-search-container">
-        <h2 className="search-title">SEARCH NOW</h2>
+        
 
         <div className="filter-buttons">
             <button className="filter-button">NEW</button>
             <button className="filter-button">USED</button>
             <button className="filter-button">SHOP ALL</button>
         </div>
-
+        
+        <div className="search-now">
+        <h2 className="search-title">SEARCH NOW</h2>
         <form className="search-form">
+            <div className="form-group">
+                
+                <select id="year" name="year">
+                    <option value="">Year</option>
+                    <option value="2024">2024</option>
+                    <option value="2023">2023</option>
+                   
+                </select>
+            </div>
+
             <div className="form-group">
                 
                 <select id="make" name="make">
@@ -28,24 +40,15 @@ function PartsSearch() {
                 
                 <select id="model" name="model">
                     <option value="">Model</option>
-                    <option value="model1">Model 1</option>
-                    <option value="model2">Model 2</option>
-                   
-                </select>
-            </div>
-
-            <div className="form-group">
-                
-                <select id="year" name="year">
-                    <option value="">Year</option>
-                    <option value="2020">2020</option>
-                    <option value="2021">2021</option>
+                    <option value="model1">model 1</option>
+                    <option value="model2">model 2</option>
                     
                 </select>
             </div>
 
             <button type="submit" className="search-button">Search</button>
         </form>
+        </div>
     </div>
 );
   
