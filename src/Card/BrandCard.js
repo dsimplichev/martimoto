0fa70c6d086da6a8b3ce77311736ac24d20fce59
dsimplichev <dsimplichev1948@gmail.com>
2024-11-05 13:45:48 +0200

    when click on brand to navigate to new page with all category

--- src/Card/BrandCard.js
+++ src/Card/BrandCard.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './brand.css';
+import { Link } from 'react-router-dom';
 
 function BrandCard({ img, brandName }) {
     return (
+        <Link to={`/brands/${brandName}`}>
         <div className="category-card">
             <img src={img} alt={brandName} />
             <p>{brandName}</p>
         </div>
+        </Link>
     );
 }
 
