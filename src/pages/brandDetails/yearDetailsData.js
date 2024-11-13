d385d959d15590bc0449fcdc295f6a832fbe3331
dsimplichev <dsimplichev1948@gmail.com>
2024-11-13 19:56:15 +0200

    need to add img

--- src/pages/brandDetails/yearDetailsData.js
+++ src/pages/brandDetails/yearDetailsData.js
@@ -3,6 +3,7 @@ import GSXR20012003 from "../../assets/SUZUKI/GSXR20012003.png";
 import GSXR20042005 from "../../assets/SUZUKI/GSXR20042005.png";
 import GSXR20062007 from "../../assets/SUZUKI/GSXR20062007.png";
 import GSXR20082010 from "../../assets/SUZUKI/GSXR20082010.png";
+import GSXR75019851987 from "../../assets/SUZUKI/GSXR75019851987.png";
 
 const yearDetailsData = {
     "GSX-R": {
@@ -12,6 +13,17 @@ const yearDetailsData = {
             { year: "2004-2005", img: GSXR20042005 },
             { year: "2006-2007", img: GSXR20062007 },
             { year: "2008-2010", img: GSXR20082010 },
+        ],
+        "750": [
+            { year: "1985-1987", img: GSXR75019851987 },
+            { year: "1988-1989", img: GSXR20012003 },
+            { year: "1994-1995", img: GSXR20042005 },
+            { year: "1996-1997", img: GSXR20062007 },
+            { year: "1998-2000", img: GSXR20082010 },
+            { year: "2000-2003", img: GSXR20012003 },
+            { year: "2004-2005", img: GSXR20042005 },
+            { year: "2006-2007", img: GSXR20062007 },
+            { year: "2011-2016", img: GSXR20082010 },
         ],
     },
 };
