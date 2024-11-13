78c748a471583a72a85121b2b71fdf11ddc5852c
dsimplichev <dsimplichev1948@gmail.com>
2024-11-13 22:33:38 +0200

    addding img

--- src/pages/brandDetails/yearDetailsData.js
+++ src/pages/brandDetails/yearDetailsData.js
@@ -7,9 +7,10 @@ import GSXR75019851987 from "../../assets/SUZUKI/GSXR75019851987.png";
 import GSXR19881989 from "../../assets/SUZUKI/GSXR19881989.png";
 import GSXR19941995 from "../../assets/SUZUKI/GSXR19941995.png";
 import GSXR19961997 from "../../assets/SUZUKI/GSXR19961997.png";
-import GSXR19882000 from "../../assets/SUZUKI/GSXR19882000.png";
-import GSXR19982000 from "../../assets/SUZUKI/GSXR19882000.png";
+import GSXR19982000 from "../../assets/SUZUKI/GSXR19982000.png";
 import GSXR20002003 from "../../assets/SUZUKI/GSXR20002003.png";
+import GSXR200420055 from "../../assets/SUZUKI/GSXR200420055.png";
+
 
 const yearDetailsData = {
     "GSX-R": {
@@ -25,9 +26,9 @@ const yearDetailsData = {
             { year: "1988-1989", img: GSXR19881989 },
             { year: "1994-1995", img: GSXR19941995 },
             { year: "1996-1997", img: GSXR19961997 },
-            { year: "1998-2000", img: GSXR19882000 },
+            { year: "1998-2000", img: GSXR19982000 },
             { year: "2000-2003", img: GSXR20002003 },
-            { year: "2004-2005", img: GSXR20042005 },
+            { year: "2004-2005", img: GSXR200420055 },
             { year: "2006-2007", img: GSXR20062007 },
             { year: "2011-2016", img: GSXR20082010 },
         ],
