e35361f3eaf288b1f3c4ba095616d9a363bba3a4
dsimplichev <dsimplichev1948@gmail.com>
2024-11-13 19:49:08 +0200

    GSXR 600 done

--- src/pages/brandDetails/yearDetailsData.js
+++ src/pages/brandDetails/yearDetailsData.js
@@ -1,6 +1,8 @@
 import GSXR9600 from "../../assets/SUZUKI/GSXR9600.png";
 import GSXR20012003 from "../../assets/SUZUKI/GSXR20012003.png";
 import GSXR20042005 from "../../assets/SUZUKI/GSXR20042005.png";
+import GSXR20062007 from "../../assets/SUZUKI/GSXR20062007.png";
+import GSXR20082010 from "../../assets/SUZUKI/GSXR20082010.png";
 
 const yearDetailsData = {
     "GSX-R": {
@@ -8,8 +10,8 @@ const yearDetailsData = {
             { year: "1996-2000", img: GSXR9600 },
             { year: "2001-2003", img: GSXR20012003 },
             { year: "2004-2005", img: GSXR20042005 },
-            { year: "2006-2007", img: GSXR20012003 },
-            { year: "2008-2010", img: GSXR20012003 },
+            { year: "2006-2007", img: GSXR20062007 },
+            { year: "2008-2010", img: GSXR20082010 },
         ],
     },
 };
