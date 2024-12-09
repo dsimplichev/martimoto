9ac0e2129e85e4a02529a5490f87c7d50cc8883c
dsimplichev <dsimplichev1948@gmail.com>
2024-12-09 18:42:23 +0200

    need to fix partsDetails

--- src/pages/brandDetails/YearDetails.jsx
+++ src/pages/brandDetails/YearDetails.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import yearDetailsData from './yearDetailsData';  
+import { useParams, Link } from 'react-router-dom';
+import yearDetailsData from './yearDetailsData';
 import './yearDetails.css';
 
 function YearDetails() {
-    const { brandName, modelName, subModelName } = useParams();  
-    const yearsData = yearDetailsData[modelName]?.[subModelName] || [];  
+    const { brandName, modelName, subModelName, yearRange } = useParams();  
+    const yearsData = yearDetailsData[modelName]?.[subModelName] || [];
 
     return (
         <div className="year-details">
-            <h1 className="year-title">{modelName} {subModelName}</h1>  
+            <h1 className="year-title">{modelName} {subModelName}</h1>
             <div className="year-underline"></div>
             <div className="year-grid">
                 {yearsData.map((yearItem, index) => (
                     <div key={index} className="year-card">
-                        <img src={yearItem.img} alt={yearItem.year} className="year-image" />
-            
-                        <p className="year-name">{yearItem.year}</p>
-                       
+                        <Link to={`/brands/${brandName}/models/${modelName}/${subModelName}/${yearItem.yearRange}`}>
+                            <img src={yearItem.img} alt={yearItem.yearRange} className="year-image" />
+                            <p className="year-name">{yearItem.year}</p>
+                        </Link>
                     </div>
                 ))}
             </div>
---
+++ src/pages/partsDetails/partsData.js
@@ -0,0 +1,38 @@
+const partsData = {
+"SUZUKI": {
+    "GSX-R": {
+        "600": {
+            "1996-2000": [
+                { title: "Окачване", img: "/images/suspension96.png" },
+                { title: "Двигател", img: "/images/engine96.png" },
+            ],
+            "2001-2003": [
+                { title: "Спирачки", img: "/images/brakes01.png" },
+                { title: "Седалка", img: "/images/seat01.png" },
+            ],
+            
+        },
+        "750": {
+            "1985-1987": [
+                { title: "Скорости", img: "/images/gear85.png" },
+                { title: "Амортисьори", img: "/images/shocks85.png" },
+            ],
+            
+        },
+    }, 
+},
+    "BMW": {
+        "S1000R": {
+            "2014-2016": [
+                { title: "Окачване", img: "/images/bmw_suspension.png" },
+                { title: "Двигател", img: "/images/bmw_engine.png" },
+            ],
+            "2017-2019": [
+                { title: "Спирачки", img: "/images/bmw_brakes.png" },
+                { title: "Седалка", img: "/images/bmw_seat.png" },
+            ],
+        },
+    },  
+}
+
+export default partsData;
