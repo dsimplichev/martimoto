28f8ea4e30dcfcbc653fbdbd95487ed8f62ae77a
dsimplichev <dsimplichev1948@gmail.com>
2025-01-20 13:16:26 +0200

    add FaHeart to partsdetails

--- src/pages/brandDetails/PartsDetails.jsx
+++ src/pages/brandDetails/PartsDetails.jsx
@@ -2,9 +2,7 @@ import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import partsData from './partsData';
 import './partsDetails.css';
-import { FaShoppingBasket } from "react-icons/fa";
-import { FaSearch } from "react-icons/fa";
-
+import { FaSearch, FaHeart, FaShoppingBasket } from "react-icons/fa";
 
 function PartsDetails() {
     const { brandName, modelName, subModelName, yearRange } = useParams();
@@ -12,12 +10,12 @@ function PartsDetails() {
     const [currentPage, setCurrentPage] = useState(1);
     const partsPerPage = 20;
 
-    const [selectedPart, setSelectedPart] = useState(null)
+    const [selectedPart, setSelectedPart] = useState(null);
+    const [currentImage, setCurrentImage] = useState('');
 
     const indexOfLastPart = currentPage * partsPerPage;
     const indexOfFirstPart = indexOfLastPart - partsPerPage;
     const currentParts = allParts.slice(indexOfFirstPart, indexOfLastPart);
-
 
     const nextPage = () => {
         if (indexOfLastPart < allParts.length) setCurrentPage(currentPage + 1);
@@ -29,12 +27,16 @@ function PartsDetails() {
 
     const openModal = (part) => {
         setSelectedPart(part);
+        setCurrentImage(part.img); 
     };
 
     const closeModal = () => {
         setSelectedPart(null);
     };
 
+    const handleThumbnailClick = (image) => {
+        setCurrentImage(image);
+    };
 
     return (
         <div className="parts-details">
@@ -43,14 +45,15 @@ function PartsDetails() {
             <div className="parts-grid">
                 {currentParts.map((part, index) => (
                     <div key={index} className="parts-card">
-                        <img src={part.img} alt={part.title} className="parts-image" />
-                        <p className="parts-name">{part.title}</p>
-                        <div className="parts-info">
-                            <p className="parts-price">{part.price ? `$${part.price.toFixed(2)}` : "N/A"}</p>
-                            <button className="cart-btn"><FaShoppingBasket /></button>
-                            <button className="srh-btn" onClick={() => openModal(part)} ><FaSearch /></button>
-                        </div>
+                    <FaHeart className="FaHeart" />
+                    <img src={part.img} alt={part.title} className="parts-image" />
+                    <p className="parts-name">{part.title}</p>
+                    <div className="parts-info">
+                        <p className="parts-price">{part.price ? `$${part.price.toFixed(2)}` : "N/A"}</p>
+                        <button className="cart-btn"><FaShoppingBasket /></button>
+                        <button className="srh-btn" onClick={() => openModal(part)} ><FaSearch /></button>
                     </div>
+                </div>
                 ))}
             </div>
             <div className="pagination">
@@ -61,8 +64,21 @@ function PartsDetails() {
                 <div className="modal-overlay" onClick={closeModal}>
                     <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                         <button className="modal-close-btn" onClick={closeModal}>X</button>
-                        <img src={selectedPart.img} alt={selectedPart.title} className="modal-image" />
-                        <div className="modal-detailss">
+                        <div className="modal-left">
+                            <img src={currentImage} alt={selectedPart.title} className="modal-image" />
+                            <div className="modal-thumbnails">
+                                {selectedPart.additionalImages && selectedPart.additionalImages.map((img, index) => (
+                                    <img
+                                        key={index}
+                                        src={img}
+                                        alt={`Thumbnail ${index + 1}`}
+                                        className="thumbnail"
+                                        onClick={() => handleThumbnailClick(img)}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                        <div className="modal-right">
                             <h2 className="modal-title">{selectedPart.title}</h2>
                             <p className="modal-price">
                                 {selectedPart.price !== undefined
@@ -70,14 +86,13 @@ function PartsDetails() {
                                     : "Цена не е налична"}
                             </p>
                             <p className="modal-description">{selectedPart.description || "Няма описание."}</p>
-                            </div>
                             <button className="modal-cart-btn">Добави в количката</button>
-                        
+                        </div>
                     </div>
                 </div>
             )}
-
         </div>
     );
 }
-export default PartsDetails;
+
+export default PartsDetails
--- src/pages/brandDetails/partsData.js
+++ src/pages/brandDetails/partsData.js
@@ -1,46 +1,49 @@
-import GSXR6002000clutch from "../../assets/SUZUKI/GSXR6002000clutch.png"
-import GSXR6002000brakepads from "../../assets/SUZUKI/GSXR6002000brakepads.png"
+import GSXR6002000clutch from "../../assets/SUZUKI/GSXR6002000clutch.png";
+import GSXR6002000brakepads from "../../assets/SUZUKI/GSXR6002000brakepads.png";
+
 
 const partsData = {
     "SUZUKI": {
         "GSX-R": {
             "600": {
                 "1996-2000": [
-                    { title: "Brake Pads", img: GSXR6002000brakepads },
-                    { title: "Clutch ", img: GSXR6002000clutch },
-                    { title: "Brake Pads", img: GSXR6002000brakepads },
-                    { title: "Clutch ", img: GSXR6002000clutch },
-                    { title: "Brake Pads", img: GSXR6002000brakepads },
-                    { title: "Clutch ", img: GSXR6002000clutch },
-                    { title: "Brake Pads", img: GSXR6002000brakepads },
-                    { title: "Clutch ", img: GSXR6002000clutch },
+                    {
+                        title: "Brake Pads",
+                        img: GSXR6002000brakepads,
+                        additionalImages: [GSXR6002000brakepads, GSXR6002000brakepads, GSXR6002000brakepads, GSXR6002000brakepads ], 
+                    },
+                    {
+                        title: "Clutch",
+                        img: GSXR6002000clutch,
+                        additionalImages: [GSXR6002000brakepads, GSXR6002000clutch], 
+                    },
+                    
                 ],
                 "2001-2003": [
                     { title: "Спирачки", img: "/images/brakes01.png" },
                     { title: "Седалка", img: "/images/seat01.png" },
                 ],
-                
             },
             "750": {
                 "1985-1987": [
                     { title: "Скорости", img: "/images/gear85.png" },
                     { title: "Амортисьори", img: "/images/shocks85.png" },
                 ],
-                
             },
-        }, 
+        },
     },
-        "BMW": {
-            "S1000R": {
-                "2014-2016": [
-                    { title: "Окачване", img: "/images/bmw_suspension.png" },
-                    { title: "Двигател", img: "/images/bmw_engine.png" },
-                ],
-                "2017-2019": [
-                    { title: "Спирачки", img: "/images/bmw_brakes.png" },
-                    { title: "Седалка", img: "/images/bmw_seat.png" },
-                ],
-            },
-        },  
-    }
-    export default partsData;
+    "BMW": {
+        "S1000R": {
+            "2014-2016": [
+                { title: "Окачване", img: "/images/bmw_suspension.png" },
+                { title: "Двигател", img: "/images/bmw_engine.png" },
+            ],
+            "2017-2019": [
+                { title: "Спирачки", img: "/images/bmw_brakes.png" },
+                { title: "Седалка", img: "/images/bmw_seat.png" },
+            ],
+        },
+    },
+};
+
+export default partsData;
