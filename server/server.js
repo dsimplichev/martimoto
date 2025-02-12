ac72eafd9f25c1c9670539e819d8c17cecf8ea16
dsimplichev <dsimplichev1948@gmail.com>
2025-02-12 15:36:44 +0200

    add cloudinary to my project

--- server/server.js
+++ server/server.js
@@ -5,8 +5,18 @@ const cookieParser = require('cookie-parser');
 const { PORT, MONGO_URI } = require('./config');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
-
+const cloudinary = require('cloudinary').v2;  
+const multer = require('multer');  
+const upload = multer({ dest: 'uploads/' });  
 const app = express();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+
 
 app.use(cookieParser());
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
@@ -14,6 +24,23 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
+
+
+app.post('/upload', upload.single('image'), async (req, res) => {
+    try {
+        const filePath = req.file.path;  // Пътят на временно съхранения файл
+        const result = await cloudinary.uploader.upload(filePath);  // Качване на изображението в Cloudinary
+
+        // Изпращане на URL-то на каченото изображение
+        res.status(200).json({ imageUrl: result.url });
+    } catch (error) {
+        console.error('Грешка при качването на изображението:', error);
+        res.status(500).send('Грешка при качването на изображението.');
+    }
+});
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Сървърът работи!');
