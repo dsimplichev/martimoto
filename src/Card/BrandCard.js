111afbe94e3ab04be02289193b4bbd47c5707eb3
dsimplichev <dsimplichev1948@gmail.com>
2025-02-19 15:00:09 +0200

    fixing some css on brand card

--- src/Card/BrandCard.js
+++ src/Card/BrandCard.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import './brand.css'
+import './brand.css';
 import { Link } from 'react-router-dom';
-
 
 function BrandCard({ img, brandName }) {
     return (
-        <Link to={`/brands/${brandName}`} className="no-underline">
-        <div className="category-card">
+        <Link to={`/brands/${brandName}`} className="category-card no-underline">
             <img src={img} alt={brandName} />
             <p className="brand-name">{brandName}</p>
-        </div>
         </Link>
     );
 }
