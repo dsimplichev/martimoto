505e568aef6a6332166d3d62c3209bdfad37bafe
dsimplichev <dsimplichev1948@gmail.com>
2025-03-20 14:29:36 +0200

    Order details working

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -2,18 +2,47 @@ const express = require("express");
 const router = express.Router();
 const Order = require("../models/Order");
 
-
 router.post("/create", async (req, res) => {
   try {
-    const { firstName, lastName, email, phone, deliveryMethod, city, office, companyName, companyReg, companyEIK, companyAddress, comment, cart, totalAmount } = req.body;
-    
+    const {
+      firstName,
+      lastName,
+      email,
+      phone,
+      deliveryMethod,
+      city,
+      office,
+      companyName,
+      companyReg,
+      companyEIK,
+      companyAddress,
+      comment,
+      cart,
+      totalAmount,
+    } = req.body;
+
     if (!cart || cart.length === 0) {
-      return res.status(400).json({ message: "Количката не може да бъде празна." });
+      return res
+        .status(400)
+        .json({ message: "Количката не може да бъде празна." });
     }
 
     const newOrder = new Order({
-      firstName, lastName, email, phone, deliveryMethod, city, office, companyName, companyReg, companyEIK, companyAddress, comment, cart, totalAmount,
-      status: "Pending" 
+      firstName,
+      lastName,
+      email,
+      phone,
+      deliveryMethod,
+      city,
+      office,
+      companyName,
+      companyReg,
+      companyEIK,
+      companyAddress,
+      comment,
+      cart,
+      totalAmount,
+      status: "Pending",
     });
 
     const savedOrder = await newOrder.save();
@@ -24,7 +53,6 @@ router.post("/create", async (req, res) => {
   }
 });
 
-
 router.get("/", async (req, res) => {
   try {
     const orders = await Order.find();
@@ -34,8 +62,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-
-router.get('/pending', async (req, res) => {
+router.get("/pending", async (req, res) => {
   try {
     const orders = await Order.find({ status: "Pending" });
     res.json(orders);
@@ -45,8 +72,7 @@ router.get('/pending', async (req, res) => {
   }
 });
 
-
-router.patch('/update/:id', async (req, res) => {
+router.patch("/update/:id", async (req, res) => {
   try {
     const { status } = req.body;
     const updatedOrder = await Order.findByIdAndUpdate(
@@ -56,25 +82,28 @@ router.patch('/update/:id', async (req, res) => {
     );
     res.json(updatedOrder);
   } catch (error) {
-    console.error('Грешка при актуализиране на поръчката:', error);
-    res.status(500).json({ message: 'Грешка при актуализиране на поръчката.' });
+    console.error("Грешка при актуализиране на поръчката:", error);
+    res.status(500).json({ message: "Грешка при актуализиране на поръчката." });
   }
 });
 
-
 router.get("/:orderId", async (req, res) => {
   const { orderId } = req.params;
-  
+
   try {
-      const order = await Order.findById(orderId)
-      .populate("cart.productId", "title image");
-     
-      if (!order) {
-          return res.status(404).json({ message: "Поръчката не е намерена" });
-      }
-      res.json(order);
+    const order = await Order.findById(orderId)
+      .populate({
+        path: "cart.productId",
+        select: "title image",
+      })
+      .exec();
+
+    if (!order) {
+      return res.status(404).json({ message: "Поръчката не е намерена" });
+    }
+    res.json(order);
   } catch (error) {
-      res.status(500).json({ message: "Грешка при търсене на поръчката" });
+    res.status(500).json({ message: "Грешка при търсене на поръчката" });
   }
 });
 
