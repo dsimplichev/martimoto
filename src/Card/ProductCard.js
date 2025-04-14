a32d3af27f8f8860ab19313294056fcfe92b7854
dsimplichev <dsimplichev1948@gmail.com>
2025-04-15 00:49:12 +0300

    details on last product working

--- src/Card/ProductCard.js
+++ src/Card/ProductCard.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './product.css'
 
 
 
-function ProductCard({ img, title }) {
+function ProductCard({ img, title, id }) {
+    const navigate = useNavigate();
     
-    
-    
+    const handleNavigate = () => {
+        navigate(`/accessories/detail/${id}`); 
+    };
+
     return (
         <div className="product">
             <img src={img} alt={title} />
             <p>{title}</p>
             <div className="product-buttons">
-                <button className="details-btn1">Разгледай</button>
+                <button className="details-btn1" onClick={handleNavigate} >Разгледай</button>
                 <button className="add-to-cart-btn1">Добави в количка</button>
             </div>
         </div>
