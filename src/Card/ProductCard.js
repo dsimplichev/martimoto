875038e7f971f4e260a25c958e25f65d472e309e
dsimplichev <dsimplichev1948@gmail.com>
2025-06-16 12:11:20 +0300

    working on productCard

--- src/Card/ProductCard.js
+++ src/Card/ProductCard.js
@@ -1,44 +1,58 @@
-import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { CartContext } from '../Context/CartContext';
-import './product.css'
-
-
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "../Context/CartContext";
+import { FaSearch } from "react-icons/fa";
+import { MdAddShoppingCart } from "react-icons/md";
+import "./product.css";
 
 function ProductCard({ img, title, id, price, type }) {
-    const navigate = useNavigate();
-    const { addToCart } = useContext(CartContext);
-    
-    const handleNavigate = () => {
-        if (type === 'part') {
-            navigate(`/parts/detail/${id}`);
-        } else {
-            navigate(`/accessories/detail/${id}`);
-        }
-    };
+  const navigate = useNavigate();
+  const { addToCart } = useContext(CartContext);
 
-    const handleAddToCart = () => {
-        const product = {
-            id: id,
-            title: title,
-            price: price,  
-            image: img,         
-        };
-        console.log("Добавям в количката:", product);
-        addToCart(product); 
-        alert('Продуктът беше добавен в количката!');
-    };
+  const handleNavigate = () => {
+    if (type === "part") {
+      navigate(`/parts/detail/${id}`);
+    } else {
+      navigate(`/accessories/detail/${id}`);
+    }
+  };
 
-    return (
-        <div className="product">
-            <img src={img} alt={title} />
-            <p className='card-titel-product' >{title}</p>
-            <div className="product-buttons">
-                <button className="details-btn1" onClick={handleNavigate} >Разгледай</button>
-                <button className="add-to-cart-btn1" onClick={handleAddToCart} >Добави в количка</button>
-            </div>
+  const handleAddToCart = () => {
+    const product = {
+      id: id,
+      title: title,
+      price: price,
+      image: img,
+    };
+    console.log("Добавям в количката:", product);
+    addToCart(product);
+    alert("Продуктът беше добавен в количката!");
+  };
+
+  return (
+    <div className="product">
+      <img src={img} alt={title} />
+      <div className="card-titel-product">
+        {title}
+        <div className="product-buttons">
+          <button
+            className="details-btn1"
+            onClick={handleNavigate}
+            title="Разгледай"
+          >
+            <FaSearch />
+          </button>
+          <button
+            className="add-to-cart-btn1"
+            onClick={handleAddToCart}
+            title="Добави в количката"
+          >
+            <MdAddShoppingCart />
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default ProductCard;
