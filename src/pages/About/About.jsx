66ed9a2df6567454e82053c7c293317d6182f677
dsimplichev <dsimplichev1948@gmail.com>
2025-09-23 17:06:52 +0300

    fixing css

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,33 +1,29 @@
 import React, { useState } from 'react';
-import './about.css'; 
-import about from "../../assets/about.png"
+import './about.css';
+import about from "../../assets/about.png"; 
 import ContactForm from '../contact/ContactForm';
 
-
 function About() {
- 
-  const [showContactForm, setShowContactForm] = useState(false);
-   
   
+  const [showContactForm, setShowContactForm] = useState(false);
+
   return (
     <div id="about-section" className="about-container">
-      <div className="about-text">
-        <h2>About Us</h2>
+      <div className="about-content">
+        <h2 className="about-title">За нас</h2>
         <div className="divider-about"></div>
-        <p>
-          We are a company dedicated to providing the best second-hand motorcycle parts. 
-          Our mission is to offer high-quality products at affordable prices, helping motorcycle enthusiasts to find the right parts for their bikes.
+        <p className="about-text-p">
+          Ние сме компания, посветена на предоставянето на най-добрите части втора употреба за мотоциклети. Нашата мисия е да предлагаме висококачествени продукти на достъпни цени, помагайки на мотоциклетните ентусиасти да намерят правилните части за техните машини.
         </p>
-        <p>
-          Our journey started with a passion for motorcycles, and over the years we have grown to become a trusted source for parts across different brands and models.
+        <p className="about-text-p">
+          Нашето пътешествие започна с голяма страст към мотоциклетите и през годините изградихме доверен източник за части от различни марки и модели.
         </p>
-        <button className="get-in-touch-btn" onClick={() => setShowContactForm(true)}>Get in Touch</button> 
-        {showContactForm && <ContactForm onClose={() => setShowContactForm(false)} />}
+        
+        <button className="get-in-touch-btn" onClick={() => setShowContactForm(true)}>Свържете се с нас</button>
       </div>
 
-      <div className="about-image">
-        <img src={about} alt="About" />
-      </div>
+      
+      {showContactForm && <ContactForm onClose={() => setShowContactForm(false)} />}
     </div>
   );
 }
